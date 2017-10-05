de8b19e8492aac3e11797386512d36e1aae5b7b2
Brett <CrazyLlama@users.noreply.github.com>
2017-10-05 10:53:04 +0100

    Update Print_cwd.sh
    Comments are key

--- Print_cwd.sh
+++ Print_cwd.sh
@@ -3,10 +3,14 @@
 ## Should probably make the "libexec" portion take user input && increase output to exe as well
 ## Also need to add user options and whether they want anything removing just to make lives easier
 
-array=( $(ps faux | grep libexec | awk '{print $2}' | sed 's/^/\/proc\//g') )
+# Stick processes output into array
+array=( $(ps faux | grep smtp | grep -v root | awk '{print $2}' | sed 's/^/\/proc\//g') )
 
+# https://media.giphy.com/media/26DOs997h6fgsCthu/giphy.gif
 echo ${array[@]}
+echo ""
 
+# Then give it to the user
 for i in ${array[@]};
 do
     echo $(ls -al $i | grep cwd | awk '{print $11}')
