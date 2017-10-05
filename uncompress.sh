110dfb53366ccc9d4ea7a45c2ba49115c4b6c221
Brett <CrazyLlama@users.noreply.github.com>
2017-10-05 15:29:16 +0100

    Update uncompress.sh

--- uncompress.sh
+++ uncompress.sh
@@ -6,11 +6,18 @@
 ## Example start of line: 
 ## `eval(gzuncompress(base64_decode('`
 
-# Base64 decode the file and output to raw zlib format
-base64 -d file.b64 > /tmp/zlib.raw
+array=( (grep '((eval.*(base64_decode|gzinflate|\$_))|\$[0O]{4,}|FilesMan|JGF1dGhfc|IIIl|die\(PHP_OS|posix_getpwuid|Array\(base64_decode|document\.write\("\\u00|sh(3(ll|11)))' . -lroE --include=*.php*) )
 
-# Prepend gzip header and uncompress
-printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - /tmp/zlib.raw | gzip -dc > /tmp/out.txt
+# Base64 decode the file and output to raw zlib format, then prepend gzip header and uncompress
+
+for i in $array[@]; 
+do
+  grep
+done
+
+base64 -d file.b64 | printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - /tmp/zlib.raw | gzip -dc > /tmp/out.txt
 
 # What's it got?
 cat /tmp/out.txt
+
+echo "see /tmp/out.txt for full output"
