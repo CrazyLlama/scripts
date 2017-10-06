bd5d8bf0762e7747fd6f2ec7477b1b94b37cc46f
Brett <CrazyLlama@users.noreply.github.com>
2017-10-06 09:50:58 +0100

    Update smtp-enum.py

--- smtp-enum.py
+++ smtp-enum.py
@@ -14,11 +14,14 @@ for nline in infile.readlines():
 
  # Create Socket
  s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
  # Connect to the Server
  connect=s.connect((sys.argv[2],25))
+ 
  # Receive the banner
  banner=s.recv(1024)
  print banner
+ 
  # VRFY a user
  s.send('VRFY ' + linen[0] + '\r\n')
  result=s.recv(1024)
