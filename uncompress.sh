c31a0e597426f4cadee431164965c8d6c2691fe6
Brett <CrazyLlama@users.noreply.github.com>
2017-10-06 11:08:18 +0100

    Update uncompress.sh

--- uncompress.sh
+++ uncompress.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
 ## Need to add user input for file parsing or even pulling the needful out automatically
-## This only works with `gzuncompress(base64_decode(` for now
+## For now it finds limited types of files and attempts to decode them
 
 ## Example start of line: 
 ## `eval(gzuncompress(base64_decode('`
 
-array=( (grep '((eval.*(base64_decode|gzinflate|\$_))|\$[0O]{4,}|FilesMan|JGF1dGhfc|IIIl|die\(PHP_OS|posix_getpwuid|Array\(base64_decode|document\.write\("\\u00|sh(3(ll|11)))' . -lroE --include=*.php*) )
+# Put API key here:
+API="<API-KEY-HERE>"
 
-# Base64 decode the file and output to raw zlib format, then prepend gzip header and uncompress
+alias find_evil="grep '((eval.*(base64_decode|gzinflate|\$_))|\$[0O]{4,}|FilesMan|JGF1dGhfc|IIIl|die\(PHP_OS|posix_getpwuid|Array\(base64_decode|document\.write\("\\u00|sh(3(ll|11)))' . -lroE --include=*.php*"
+array=( (find_evil) )
+
+# Base64 decode the files and output to raw zlib format, then prepend gzip header and uncompress
 
 for i in $array[@]; 
 do
-  grep
+  curl -i -F api_key=$API -F file=@$i http://www.unphp.net/api/v2/post > > /tmp/out.$i
 done
-
-base64 -d file.b64 | printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - /tmp/zlib.raw | gzip -dc > /tmp/out.txt
 
 # What's it got?
 cat /tmp/out.txt
 
-echo "see /tmp/out.txt for full output"
+echo "see /tmp/out.<filename> for full output"
