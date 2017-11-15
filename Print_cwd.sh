7582190235f491ce59760ab2c369e47d4576abe3
Brett <CrazyLlama@users.noreply.github.com>
2017-11-15 09:25:19 +0000

    Update Print_cwd.sh
    Added exe checks and provides details on which process aligns with each one - as well as printing out process names rather than just the PID

--- Print_cwd.sh
+++ Print_cwd.sh
@@ -7,17 +7,27 @@ echo "Enter the string you would like to search the process list for:"
 
 read proc
 
-# Stick processes output into array
-array=( $(ps faux | grep $proc | grep -v root | awk '{print $2}' | sed 's/^/\/proc\//g') )
+# Stick processes output into array - prepending `/proc/` for later usage
+proc_array=( $(ps faux | grep $proc | grep -v root | awk '{print $2}' | sed 's/^/\/proc\//g') )
+
+# Stick processes output into array - specifically the names of the processes
+array=( $(ps faux | grep $proc | grep -v root | awk '{ s = ""; for (i = 11; i <= NF; i++) s = s $i " "; print s }')
 
 # https://media.giphy.com/media/26DOs997h6fgsCthu/giphy.gif
+echo ""
+echo "Applicable processes:"
 echo ${array[@]}
 echo ""
 
-# Then give it to the user
-for i in ${array[@]};
+# Then give both cwd and exe to the user
+echo "Current working directories and executables:"
+
+for i in ${proc_array[@]};
 do
-    echo $(ls -al $i | grep cwd | awk '{print $11}')
+    echo "process directory - $i"
+    echo "cwd - $(ls -al $i | grep cwd | awk '{print $11}')"
+    echo "exe - $(ls -al $i | grep exe | awk '{print $11}')"
+    echo ""
 done
 
 ## Remotely executing this: bash <(curl -s <RAW-LINK>)
