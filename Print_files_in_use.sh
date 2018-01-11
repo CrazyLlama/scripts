6aa1b7237113efe72a98ce040822a55977ddbf91
Brett <CrazyLlama@users.noreply.github.com>
2018-01-11 09:44:08 +0000

    Update Print_files_in_use.sh

--- Print_files_in_use.sh
+++ Print_files_in_use.sh
@@ -4,7 +4,7 @@ proc_array=( $(ps faux | awk '{print $2}' | grep -v "PID") )
 
 for i in ${proc_array[@]};
 do
-	echo ""
+    echo ""
     echo "process name - $(ps faux | grep $i | awk '{print $11}' | grep -v '\_')"
     echo "files in use by process - $(lsof –Pnp $i | grep -Ev 'pipe|/dev/null')"
     echo ""
